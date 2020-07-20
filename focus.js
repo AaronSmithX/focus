4e9d37ec3ed34c5aebbfd75f46732238a0b7b97d
Aaron Smith <aaron.smith@bvaccel.com>
2020-07-20 13:15:17 -0400

    Support iframes

--- focus.js
+++ focus.js
@@ -1,11 +1,15 @@
 (Focus => {
 
+  const focusHiddenElements = new Set();
   const zones = new Set();
   let activeZone = null;
   let lastActiveElement = null;
 
+  // all elements that can receive focus
+  // iframe is included so it can be specially handled,
+  // as iframes provide unique challenges to manually controlling focus
   const focusQuery =
-    ['button', 'a[href]', 'area[href]', 'input', 'select', 'textarea', '[tabindex]']
+    ['button', 'a[href]', 'area[href]', 'input', 'select', 'textarea', '[tabindex]', 'iframe'] // iframe for special handling
     .map(target => target + ':not([tabindex="-1"]):not([disabled])')
     .join(', ');
 
@@ -24,99 +28,98 @@
 
   const tabbableChildren = element =>
     focusableChildren(element).filter(isTabbable);
+  
+  const hideElementFromFocus = element => {
+    const originalTabIndex = element.getAttribute('tabindex');
+    if (originalTabIndex) {
+      element.dataset.originalTabIndex = originalTabIndex;
+    }
+    element.setAttribute('tabindex', -1);
+    focusHiddenElements.add(element);
+  };
+
+  const restoreFocusHiddenElements = () => {
+    for (const element of focusHiddenElements) {
+      const { originalTabIndex } = element.dataset;
+      if (originalTabIndex) {
+        element.setAttribute('tabindex', element.dataset.originalTabIndex);
+      }
+      else {
+        element.removeAttribute('tabindex');
+      }
+      delete element.dataset.originalTabIndex;
+    }
+    focusHiddenElements.clear();
+  };
+
+  const hideExternalElementsFromFocus = zoneElement => {
+    const tabbableElements = tabbableChildren(document.body);
+    for (const element of tabbableElements) {
+      if (zoneElement !== element && !zoneElement.contains(element)) {
+        hideElementFromFocus(element);
+      }
+    }
+  };
+
+  const hideAllZonesFromFocus = () => {
+    for (const zoneElement of zones) {
+      const tabbableZoneChildren = tabbableChildren(zoneElement);
+      for (const element of tabbableZoneChildren) {
+        hideElementFromFocus(element);
+      }
+    }
+  };
+
+  const getZoneEdges = (element) => {
+    const bumperStart = activeZone.querySelector('[data-zone-role="bumper-start"]');
+    const bumperEnd = activeZone.querySelector('[data-zone-role="bumper-end"]');
+    const elementTabbableChildren = tabbableChildren(activeZone).filter(element => !element.dataset.zoneRole);
+    const firstFocusChild = elementTabbableChildren[0];
+    const lastFocusChild = elementTabbableChildren[elementTabbableChildren.length - 1];
+    return { bumperStart, bumperEnd, firstFocusChild, lastFocusChild };
+  }
 
   const focusZone = element => {
     lastActiveElement = document.activeElement;
+    restoreFocusHiddenElements();
+    hideExternalElementsFromFocus(element);
     activeZone = element;
     if (element.matches(focusQuery)) {
       element.focus();
     }
     else {
-      elementTabbableChildren = tabbableChildren(element);
-      if (elementTabbableChildren.length) elementTabbableChildren[0].focus();
+      const { firstFocusChild } = getZoneEdges(activeZone);
+      firstFocusChild.focus();
     }
   };
 
   const blurZone = element => {
     if (activeZone === element) {
+      restoreFocusHiddenElements();
+      hideAllZonesFromFocus();
       activeZone = null;
       (lastActiveElement || tabbableChildren(document.body)[0]).focus();
     }
   };
 
-  const ensureZoneFocused = event => {
-    if (!activeZone.contains(event.target)) {
-      focusZone(activeZone);
-      return false;
-    }
-    return true;
-  }
-
-  const activeZonePrevious = event => {
-    if (!ensureZoneFocused(event)) return;
-    const tabbableZoneElements = tabbableChildren(activeZone);
-    const index = tabbableZoneElements.indexOf(event.target);
-    let previousIndex = index - 1;
-    if (previousIndex === -1) previousIndex = tabbableZoneElements.length - 1;
-    tabbableZoneElements[previousIndex].focus();
-  };
-
-  const activeZoneNext = event => {
-    if (!ensureZoneFocused(event)) return;
-    const tabbableZoneElements = tabbableChildren(activeZone);
-    const index = tabbableZoneElements.indexOf(event.target);
-    let nextIndex = index + 1;
-    if (nextIndex === tabbableZoneElements.length) nextIndex = 0;
-    tabbableZoneElements[nextIndex].focus();
-  };
-
-  const allTabbableZoneElements = () => {
-    let tabbableZoneElements = [];
-    for (let zone of zones) {
-      tabbableZoneElements = [...tabbableZoneElements, ...tabbableChildren(zone)];
-    }
-    return tabbableZoneElements;
-  };
-
-  const allTabbableOuterElements = () => {
-    let tabbableZoneElements = allTabbableZoneElements();
-    let tabbableElements = tabbableChildren(document.body);
-    tabbableOuterElements = tabbableElements.filter(element => {
-      return !tabbableZoneElements.includes(element);
-    });
-    return tabbableOuterElements;
-  }
-
-  const outerFocusPrevious = event => {
-    const tabbableOuterElements = allTabbableOuterElements();
-    if (!tabbableOuterElements.includes(event.target)) {
-      return tabbableOuterElements[0].focus();
-    }
-    const index = tabbableOuterElements.indexOf(event.target);
-    let previousIndex = index - 1;
-    if (previousIndex === -1) previousIndex = tabbableOuterElements.length - 1;
-    tabbableOuterElements[previousIndex].focus();
-  };
-
-  const outerFocusNext = event => {
-    const tabbableOuterElements = allTabbableOuterElements();
-    if (!tabbableOuterElements.includes(event.target)) {
-      return tabbableOuterElements[0].focus();
-    }
-    const index = tabbableOuterElements.indexOf(event.target);
-    let nextIndex = index + 1;
-    if (nextIndex === tabbableOuterElements.length) nextIndex = 0;
-    tabbableOuterElements[nextIndex].focus();
-  };
-
   const shiftTabHandler = event => {
-    if (activeZone) activeZonePrevious(event);
-    else outerFocusPrevious(event);
+    if (activeZone) {
+      const { bumperStart, firstFocusChild, lastFocusChild } = getZoneEdges(activeZone);
+      if (event.target === bumperStart || event.target === firstFocusChild) {
+        event.preventDefault();
+        lastFocusChild.focus();
+      }
+    }
   };
 
   const tabHandler = event => {
-    if (activeZone) activeZoneNext(event);
-    else outerFocusNext(event);
+    if (activeZone) {
+      const { bumperEnd, firstFocusChild, lastFocusChild } = getZoneEdges(activeZone);
+      if (event.target === bumperEnd || event.target === lastFocusChild) {
+        event.preventDefault();
+        firstFocusChild.focus();
+      }
+    }
   };
 
   const escapeHandler = () => {
@@ -125,7 +128,7 @@
 
   const keydownListener = event => {
     if (event.keyCode === 9) {
-      event.preventDefault();
+      // event.preventDefault();
       if (event.shiftKey) {
         shiftTabHandler(event);
       }
@@ -139,9 +142,41 @@
   };
 
   document.addEventListener('keydown', keydownListener);
+
+  const addBumpers = element => {
+    const bumperStart = document.createElement('input');
+    bumperStart.style.opacity = '0';
+    bumperStart.style.position = 'absolute';
+    bumperStart.dataset.zoneRole = 'bumper-start';
+    element.prepend(bumperStart);
+    bumperStart.addEventListener('focus', function() {
+      const { lastFocusChild } = getZoneEdges(activeZone);
+      lastFocusChild.focus();
+    });
+
+    const bumperEnd = document.createElement('input');
+    bumperEnd.style.opacity = '0';
+    bumperEnd.style.position = 'absolute';
+    bumperEnd.dataset.zoneRole = 'bumper-end';
+    element.append(bumperEnd);
+    bumperEnd.addEventListener('focus', function() {
+      const { firstFocusChild } = getZoneEdges(activeZone);
+      firstFocusChild.focus();
+    });
+  };
+
+  const removeBumpers = element => {
+    const bumperStart = element.querySelector('[zone-role="bumper-start"]');
+    bumperStart.remove();
+    const bumperEnd = element.querySelector('[zone-role="bumper-end"]');
+    bumperEnd.remove();
+  };
   
   Focus.createZone = element => {
+    restoreFocusHiddenElements();
+    addBumpers(element);
     zones.add(element);
+    hideAllZonesFromFocus();
     return {
       focus: () => focusZone(element),
       blur: () => blurZone(element),
@@ -149,7 +184,10 @@
   };
 
   Focus.destroyZone = element => {
-    return zones.delete(element);
+    restoreFocusHiddenElements();
+    removeBumpers(element);
+    zones.delete(element);
+    hideAllZonesFromFocus();
   };
 
 })(window.Focus = {});
